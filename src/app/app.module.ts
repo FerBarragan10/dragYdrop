245b43f54ab080ed8cef7981374f2908e62a608a
fernando.barragan <fernando.barragan@sdc.com.ar>
2020-01-02 14:47:55 -0300

    primer commiteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{DragDropModule} from '@angular/cdk/drag-drop';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    DragDropModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/lista/lista.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {PaisesService} from '../../services/paises.service';
+import { CdkDragDrop,moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent implements OnInit {
+   paises:any=[];
+   paisesSudamerica:any=[];
+   otrosPaises:any=[];
+
+  constructor(private service:PaisesService) { }
+
+  ngOnInit() {
+    this.getPaises();
+  }
+  getPaises(){
+    this.service.getPaises().subscribe(data=>{
+        console.log(data);
+        this.paises=data;
+    });
+  }
+  drop(evento:CdkDragDrop<any>){
+  
+    console.log("ok!",evento);
+    if(evento.previousContainer!==evento.container){
+     
+      transferArrayItem(evento.previousContainer.data,evento.container.data,evento.previousIndex,evento.currentIndex);
+    }else{
+      moveItemInArray(this.paises,evento.previousIndex,evento.currentIndex);
+
+  
+    }
+  }
+}
